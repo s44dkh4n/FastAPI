091b886e2422edd33c47e218eeb673d71898ee2a
Saad Khan <iams44dkh4n5051@gmail.com>
2026-08-22 16:13:07 +0500

    Resolved Problems

--- ML_API.py
+++ ML_API.py
@@ -1,7 +1,7 @@
 from fastapi import FastAPI, HTTPException
 from fastapi.responses import JSONResponse
 from schema.userinput import UserInfo
-from model.predict import predict_output,model,MODEL_VERSION
+from model.predict import predict_output, model, MODEL_VERSION
 
 app = FastAPI()
 
--- model/predict.py
+++ model/predict.py
@@ -1,7 +1,7 @@
 import pickle
 import pandas as pd
 
-with open(r"FastAPI\model\model.pkl", "rb") as f:
+with open("model/model.pkl", "rb") as f:
     model = pickle.load(f)
 
 MODEL_VERSION = "1.0.1"
